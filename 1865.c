44fca51fc101a49d4542d07e9f84027978331630
DraftBugle2 <127749741+DraftBugle2@users.noreply.github.com>
2023-11-24 18:49:38 -0300

    Add files via upload

---
+++ 1865.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void ehOThor(char heroi[]){
+    if(strcmp(heroi, "Thor") == 0){
+        printf("Y\n");
+    } else {
+        printf("N\n");
+    }
+}
+
+int main(){
+    int numEntradas, forca;
+    char heroi[100];
+    scanf("%d", &numEntradas);
+
+    for(int i = 0;i < numEntradas;i++){
+        scanf("%s", heroi);
+        scanf("%d", &forca);
+            ehOThor(heroi);
+    }
+
+    return 0;
+}
